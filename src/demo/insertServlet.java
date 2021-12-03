6971784419fb7615e196685674c5c9fec9915f00
Xi-jun <939161668@qq.com>
2021-12-03 23:30:40 +0800

    舍弃图片判断功能

--- src/demo/insertServlet.java
+++ src/demo/insertServlet.java
@@ -2,15 +2,10 @@ package demo;
 
 import dao.staffinfoDao;
 import implement.staffinfoImpl;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @WebServlet(name = "staffinsertServlet",
         urlPatterns = {"/staffinsertServlet.action"})
@@ -23,23 +18,7 @@ public class insertServlet extends HttpServlet {
             String staffid=request.getParameter("number");
             String staffname=request.getParameter("username");
             String stationid=request.getParameter("title");
-            Boolean isAble=false;
-            //判断是否上传东西
-            FileItemFactory factory = new DiskFileItemFactory();
-            //创建文件上传的处理器
-            ServletFileUpload upload = new ServletFileUpload(factory);
-            //解析请求
-            List<FileItem> items = upload.parseRequest(request);
-            for (FileItem item : items) {
-                String fileName = item.getFieldName();
-                if (!item.isFormField()) {//前端保证不空
-                    String value = item.getString("utf-8");
-                    String uploadname = item.getFieldName();
-                    if (uploadname == "demo2") {
-                        isAble=true;//资格证
-                    }
-                }
-            }
+            Boolean isAble=true;
             dao.insert(staffid,staffname,stationid,isAble);
                 } catch(Exception e){
                     e.printStackTrace();
