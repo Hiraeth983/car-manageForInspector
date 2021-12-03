b82daac846519b26f3dcfe6a9c69552a8014d697
Pounds-zt <1102168414@qq.com>
2021-12-04 00:17:41 +0800

    修改查看任务daoimpl

--- src/implement/missionImpl.java
+++ src/implement/missionImpl.java
@@ -19,40 +19,24 @@ public class missionImpl implements missionDao {
         try {
             Connection conn = BaseDao.getConnection();
             stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-
             ResultSet rs = null;
-            try {
-                rs = stmt.executeQuery();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-            while (true) {
-                try {
-                    if (!rs.next()) break;
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-                try {
-                    mission tem = new mission();
-                    tem.setOrderId(rs.getString("orderId"));
-                    tem.setIdcard(rs.getString("idcard"));
-                    tem.setFullname(rs.getString("fullname"));
-                    tem.setCarId(rs.getString("carId"));
-                    tem.setAddress(rs.getString("address"));
-                    tem.setOrderTime(rs.getString("orderTime"));
-                    tem.setCheckTime(rs.getString("checkTime"));
-                    tem.setResult(rs.getString("result"));
-                    tem.setStaffname(rs.getString("staffname"));
-                    tem.setStaffId(rs.getString("staffId"));
-                    tem.setScore(rs.getInt("score"));
-                    tem.setProcess(rs.getString("process"));
-                    tem.setAvailable(rs.getBoolean("available"));
-                    list.add(tem);
-                } catch (SQLException e3) {
-                    e3.printStackTrace();
-                }
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                mission tem = new mission();
+                tem.setOrderId(rs.getString("orderId"));
+                tem.setIdcard(rs.getString("idcard"));
+                tem.setFullname(rs.getString("fullname"));
+                tem.setCarId(rs.getString("carId"));
+                tem.setAddress(rs.getString("address"));
+                tem.setOrderTime(rs.getString("orderTime"));
+                tem.setCheckTime(rs.getString("checkTime"));
+                tem.setResult(rs.getString("result"));
+                tem.setStaffname(rs.getString("staffname"));
+                tem.setStaffId(rs.getString("staffId"));
+                tem.setScore(rs.getInt("score"));
+                tem.setProcess(rs.getString("process"));
+                tem.setAvailable(rs.getBoolean("available"));
+                list.add(tem);
             }
         } catch (Exception e) {
             e.printStackTrace();
